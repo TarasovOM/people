822ad611898bcf619e13cc25c2385aeadec14129
Tarasov Oleg <tarasovom@mail.ru>
2024-05-05 09:56:08 +0300

    modifi

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -19,11 +20,24 @@ public class Main {
             );
         }
         long count = persons.stream()
-                .filter(x -> x.getAge() < 18)
+                .filter(person -> person.getAge() < 18)
                 .count();
-        System.out.println(count);
+        System.out.println("Количество несовершенолетних " + count);
 
-        
 
+        List<String> conscripts = persons.stream()
+                .filter(person -> person.getSex() == Sex.MAN && person.getAge() >= 18 && person.getAge() <= 27)
+                .map(Person::getFamily)
+                .toList();
+        System.out.println("Список призывников: " + conscripts);
+
+
+        List<Person> Workers = persons.stream()
+                .filter(person -> (person.getSex() == Sex.WOMAN && person.getAge() >= 18 && person.getAge() <= 60) ||
+                        (person.getSex() == Sex.MAN && person.getAge() >= 18 && person.getAge() <= 65))
+                .filter(person -> person.getEducation() == Education.HIGHER)
+                .sorted(Comparator.comparing(Person::getFamily))
+                .toList();
+        System.out.println("Список работоспособных людей: " + Workers);
     }
 }
